ef51926daa0c6a7d221fd5d5e167f794a7a3c7e1
Svetlana Khaydoshkina <sveta13131313@yandex.ru>
2021-07-30 17:55:03 +0300

    Выполненное дз

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let money; 
-let income; 
-let addExpenses; 
-let deposit; 
-let mission; 
-let period;
+//Задание 1
+let money = 300000;
+let income = '50000';
+let addExpenses = ' Интернет 1000, Такси 2500, Коммуналка 5500';
+let deposit = true;
+let mission = 15000000;
+let period = 10;
 
-alert('Hello world!');
 
-console.log('Good day');
+//Задание 2
+
+console.log('money = ', typeof money);
+console.log('income = ', typeof income);
+console.log('deposit = ', typeof deposit);
+
+console.log('Длина строки = ', addExpenses.length);
+
+console.log('Массив ',addExpenses.toLowerCase().split(', '));
+
+console.log('Период равен ', period, ' месяцев');
+console.log('Цель заработать ', mission, ' рублей');
+
+//дневной бюджет
+let budgetDay = money / 30;
+console.log('Дневной бюджет = ', budgetDay);
